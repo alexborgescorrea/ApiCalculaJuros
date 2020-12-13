be7963eb6c52770036b5f904e016e4b331367216
Alexsandro Borges Correa <alexborgescorrrea@gmail.com>
2020-12-13 11:43:52 -0300

    Criado teste unitário para a classe TaxaJurosAplic

---
+++ ApiCalculaJuros.Test/Aplicacao/TaxaJuros/TaxaJurosAplicTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApiCalculaJuros.Test.Aplicacao.TaxaJuros
+{
+    public class TaxaJurosAplicTest
+    {
+        [Fact]
+        public void TaxaPadrao_TestaValorRetornado()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ ApiCalculaJuros/Aplicacao/TaxaJuros/TaxaJurosAplic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculaJuros.Aplicacao.TaxaJuros
+{
+    public class TaxaJurosAplic
+    {
+        public async Task<decimal> TaxaPadraoAsync()
+        {
+            return await Task.FromResult(0);
+        }
+    }
+}
