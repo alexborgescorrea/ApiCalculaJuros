2111c070bf511d2153b29a5fb97bb7a1faed9810
Alexsandro Borges Correa <alexborgescorrrea@gmail.com>
2020-12-13 11:51:12 -0300

    Implementação do método TaxaJurosAplic.TaxaPadraoAsybc()

--- ApiCalculaJuros/Aplicacao/TaxaJuros/TaxaJurosAplic.cs
+++ ApiCalculaJuros/Aplicacao/TaxaJuros/TaxaJurosAplic.cs
@@ -1,15 +1,39 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace ApiCalculaJuros.Aplicacao.TaxaJuros
 {
     public class TaxaJurosAplic
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public TaxaJurosAplic(IHttpClientFactory httpClientFactory, 
+                              IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
         public async Task<decimal> TaxaPadraoAsync()
         {
-            return await Task.FromResult(0);
+            var httpClient = _httpClientFactory.CreateClient();
+
+            var resp = await httpClient.GetAsync(GetUrl());
+
+            resp.EnsureSuccessStatusCode();
+
+            return await JsonSerializer.DeserializeAsync<decimal>(await resp.Content.ReadAsStreamAsync());
+        }
+
+        private Uri GetUrl()
+        {
+            return new Uri(_configuration.GetValue<string>("UrlApiTaxaJuros"));
         }
     }
 }
